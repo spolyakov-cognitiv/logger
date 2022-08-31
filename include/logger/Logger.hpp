8db1463edf566b347dfacb2d29077ebd6214a808
Sergey Polyakov <spolyakov@cognitiv.ai>
2022-08-31 13:13:17 -0700

    [System] First working Logger version

--- include/logger/Logger.hpp
+++ include/logger/Logger.hpp
@@ -5,11 +5,7 @@
 #include <string>
 #include <vector>
 
-namespace spdlog
-{
-// Forward declaration
-class logger;
-} // namespace spdlog
+//#include <experimental/source_location> EXIST gcc-10
 
 namespace cognitiv
 {
@@ -25,39 +21,47 @@ enum class LogLevel : uint8_t {
 	COUNT //< Maximum value, used for range check
 };
 
-enum class SinkType : uint8_t {
+namespace sink
+{
+enum class Type : uint8_t {
 	CONSOLE,
 	FILE,
 	DLT, // Hope, we will use it in the future for remote logs
 	SYSLOG // For metrics and remote logs possibility
 };
 
-struct SinkConfig {
-	SinkType type{SinkType::CONSOLE};
+struct Config {
+	Type type{Type::CONSOLE};
 	LogLevel log_level{LogLevel::TRACE};
 
+	// TODO rethink
 	std::optional<uint32_t> log_file_max_count{std::nullopt};
 	std::optional<size_t> log_file_max_size_mb{std::nullopt};
 	std::optional<std::string> log_directory{std::nullopt};
 };
+} // namespace sink
 
-struct LoggerConfig {
+namespace logger
+{
+struct Config {
 	std::string name;
 	LogLevel log_level{LogLevel::TRACE};
-	std::vector<SinkConfig> sinks;
+	std::vector<sink::Config> sinks;
 };
 
-class Logger
+class ILogger
 {
 public:
-	Logger(const LoggerConfig& config);
+	virtual ~ILogger() = default;
 
-	// Should be incapsulated later
-	std::shared_ptr<spdlog::logger> register_logger(const std::string& name);
-	std::shared_ptr<spdlog::logger> get_logger(const std::string& name) const;
-
-private:
-	LoggerConfig config_;
+	// Add source location and extend formatter
+	virtual void log_msg(LogLevel level, const std::string_view& msg) = 0;
 };
+
+void init();
+std::shared_ptr<ILogger> register_new(const Config& config);
+std::shared_ptr<ILogger> get(const std::string& name);
+
+} // namespace logger
 
 } // namespace cognitiv
--- src/logger/Logger.cpp
+++ src/logger/Logger.cpp
@@ -1,4 +1,4 @@
-#include "Logger.hpp"
+#include "logger/Logger.hpp"
 
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
@@ -14,17 +14,18 @@ constexpr auto* DEFAULT_LOG_PATTERN = "[%Y-%m-%d %H:%M:%S.%e %z] [%n] [tid %t] [
 constexpr auto* DEFAULT_LOG_FILE_PATH{"/var/log/bidrouter/"};
 constexpr size_t DEFAULT_LOG_FILE_MAX_SIZE_MB{1024U * 1024 * 1024};
 constexpr uint32_t DEFAULT_LOG_FILE_MAX_COUNT{3U};
+constexpr auto DEFAULT_FLUSH_TIMEOUT{std::chrono::seconds(10)};
 
-std::vector<spdlog::sink_ptr> create_sinks(const cognitiv::LoggerConfig& config)
+std::vector<spdlog::sink_ptr> create_sinks(const cognitiv::logger::Config& config)
 {
 	std::vector<spdlog::sink_ptr> sinks;
 
 	for (auto& sink : config.sinks) {
 		switch (sink.type) {
-			case cognitiv::SinkType::CONSOLE: {
+			case cognitiv::sink::Type::CONSOLE: {
 				sinks.push_back(GLOBAL_CONSOLE_SINK);
 			} break;
-			case cognitiv::SinkType::FILE: {
+			case cognitiv::sink::Type::FILE: {
 				auto rotating_file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
 					std::filesystem::path(
 						sink.log_directory.value_or(DEFAULT_LOG_FILE_PATH) + '/' + config.name),
@@ -32,8 +33,8 @@ std::vector<spdlog::sink_ptr> create_sinks(const cognitiv::LoggerConfig& config)
 					sink.log_file_max_count.value_or(DEFAULT_LOG_FILE_MAX_COUNT));
 				sinks.push_back(rotating_file_sink);
 			} break;
-			case cognitiv::SinkType::DLT:
-			case cognitiv::SinkType::SYSLOG:
+			case cognitiv::sink::Type::DLT:
+			case cognitiv::sink::Type::SYSLOG:
 				// Doesn't supported yet
 				break;
 		}
@@ -42,7 +43,7 @@ std::vector<spdlog::sink_ptr> create_sinks(const cognitiv::LoggerConfig& config)
 	return sinks;
 }
 
-spdlog::level::level_enum to_spdlog_log_level(cognitiv::LogLevel level)
+spdlog::level::level_enum convert(cognitiv::LogLevel level)
 {
 	switch (level) {
 		case cognitiv::LogLevel::TRACE:
@@ -62,6 +63,8 @@ spdlog::level::level_enum to_spdlog_log_level(cognitiv::LogLevel level)
 		case cognitiv::LogLevel::COUNT:
 			return spdlog::level::n_levels;
 	}
+
+	return spdlog::level::trace;
 }
 
 } // namespace
@@ -69,37 +72,47 @@ spdlog::level::level_enum to_spdlog_log_level(cognitiv::LogLevel level)
 namespace cognitiv
 {
 
-Logger::Logger(const LoggerConfig& config)
-	: config_(std::move(config))
+class SpdlogLogger : public logger::ILogger
+{
+public:
+	explicit SpdlogLogger(std::shared_ptr<spdlog::logger> logger)
+		: logger_(std::move(logger))
+	{
+	}
+
+	void log_msg(LogLevel level, const std::string_view& msg) override
+	{
+		logger_->log(convert(level), msg);
+	};
+
+private:
+	std::shared_ptr<spdlog::logger> logger_{nullptr};
+};
+
+void logger::init()
 {
 	if (!GLOBAL_CONSOLE_SINK) {
 		spdlog::set_pattern(DEFAULT_LOG_PATTERN);
 		GLOBAL_CONSOLE_SINK = std::make_shared<spdlog::sinks::stdout_color_sink_st>();
 	}
-	spdlog::flush_every(std::chrono::seconds(10));
-
-	std::vector<spdlog::sink_ptr> sinks = create_sinks(config_);
-
-	auto service_logger = std::make_shared<spdlog::logger>(config_.name, begin(sinks), end(sinks));
-	service_logger->set_level(to_spdlog_log_level(config_.log_level));
-	service_logger->set_pattern(DEFAULT_LOG_PATTERN);
-	spdlog::register_logger(service_logger);
+	spdlog::flush_every(DEFAULT_FLUSH_TIMEOUT);
 }
 
-std::shared_ptr<spdlog::logger> Logger::register_logger(const std::string& name)
+std::shared_ptr<logger::ILogger> logger::register_new(const logger::Config& config)
 {
-	std::vector<spdlog::sink_ptr> sinks = create_sinks(config_);
+	std::vector<spdlog::sink_ptr> sinks = create_sinks(config);
 
-	auto logger = std::make_shared<spdlog::logger>(name, begin(sinks), end(sinks));
+	auto logger = std::make_shared<spdlog::logger>(config.name, begin(sinks), end(sinks));
+	logger->set_level(convert(config.log_level));
 	logger->set_pattern(DEFAULT_LOG_PATTERN);
 	spdlog::register_logger(logger);
 
-	return logger;
+	return std::make_shared<SpdlogLogger>(logger);
 }
 
-std::shared_ptr<spdlog::logger> Logger::get_logger(const std::string& name) const
+std::shared_ptr<logger::ILogger> logger::get(const std::string& name)
 {
-	return spdlog::get(name);
+	return std::make_shared<SpdlogLogger>(spdlog::get(name));
 }
 
 } // namespace cognitiv
